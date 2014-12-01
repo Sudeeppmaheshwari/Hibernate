366dbd8f333b92b6e22f95460895e930554becbf
indianic <indianic@10.2.4.173>
2014-12-01 12:59:14 +0530

    show how the hibernate takes the values using model class.

--- FirstHibernateProj/src/com/sudeep/hibernate/dto/UserDetails.java
+++ FirstHibernateProj/src/com/sudeep/hibernate/dto/UserDetails.java
@@ -4,15 +4,15 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity (name="USER_DETAILS")
+@Entity(name = "USER_DETAILS")
 public class UserDetails {
 
-	@Id
-	@Column (name="USER_ID")
 	private int userId;
-	@Column (name="USER_NAME")
+
 	private String userName;
 
+	@Id
+	@Column(name = "USER_ID")
 	public int getUserId() {
 		return userId;
 	}
@@ -21,8 +21,9 @@ public class UserDetails {
 		this.userId = userId;
 	}
 
+	@Column(name = "USER_NAME")
 	public String getUserName() {
-		return userName;
+		return userName + "From getter";
 	}
 
 	public void setUserName(String userName) {

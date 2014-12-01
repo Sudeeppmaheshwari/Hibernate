4533aa710ebe39915247860d58de652c09294ce4
indianic <indianic@10.2.4.173>
2014-12-01 14:19:55 +0530

    @Transient is used if we don't want to store that field into database.
    @Temporal(TemporalType.DATE)
    is used to discribe date stamp of date field for database.

--- FirstHibernateProj/src/com/sudeep/hibernate/client/clientApp.java
+++ FirstHibernateProj/src/com/sudeep/hibernate/client/clientApp.java
@@ -1,5 +1,7 @@
 package com.sudeep.hibernate.client;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -11,8 +13,11 @@ public class clientApp {
 	public static void main(String[] args) {
 
 		UserDetails userDetails = new UserDetails();
-		userDetails.setUserId(2);
-		userDetails.setUserName("Second User");
+		userDetails.setUserId(1);
+		userDetails.setUserName("First User");
+		userDetails.setAddress("test address.");
+		userDetails.setJoinedDate(new Date());
+		userDetails.setDetails("Description");
 
 		SessionFactory sessionFactory = new Configuration().configure()
 				.buildSessionFactory();
--- FirstHibernateProj/src/com/sudeep/hibernate/dto/UserDetails.java
+++ FirstHibernateProj/src/com/sudeep/hibernate/dto/UserDetails.java
@@ -1,15 +1,31 @@
 package com.sudeep.hibernate.dto;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
-@Entity(name = "USER_DETAILS")
+@Entity
+@Table(name = "USER_DETAILS")
 public class UserDetails {
 
 	private int userId;
 
 	private String userName;
+	@Temporal(TemporalType.DATE)
+	private Date joinedDate;
+	private String address;
+	private String details;
+	// This field is common for all users
+	// so that if we don't want to store this
+	// field into our database.
+	@Transient
+	public static String companyName;
 
 	@Id
 	@Column(name = "USER_ID")
@@ -28,6 +44,30 @@ public class UserDetails {
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
 	}
 
 }

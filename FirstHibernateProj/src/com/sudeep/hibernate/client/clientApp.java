057f9e8c0bbf60e5f1d860c3a12f0ac8b139e1e5
indianic <indianic@10.2.4.173>
2014-12-03 10:54:14 +0530

    Pull from master

--- FirstHibernateProj/src/com/sudeep/hibernate/client/clientApp.java
+++ FirstHibernateProj/src/com/sudeep/hibernate/client/clientApp.java
@@ -12,19 +12,46 @@ public class clientApp {
 
 	public static void main(String[] args) {
 
-		UserDetails userDetails = new UserDetails();
-		userDetails.setUserId(1);
-		userDetails.setUserName("First User");
-		userDetails.setAddress("test address.");
-		userDetails.setJoinedDate(new Date());
-		userDetails.setDetails("Description");
+		// UserDetails userDetails = new UserDetails();
+		// userDetails.setUserId(1);
+		// userDetails.setUserName("First User");
+		// userDetails.setAddress("test address.");
+		// userDetails.setJoinedDate(new Date());
+		// userDetails.setDetails("Description");
+		//
+		// SessionFactory sessionFactory = new Configuration().configure()
+		// .buildSessionFactory();
+		// Session session = sessionFactory.openSession();
+		// session.beginTransaction();
+		// session.save(userDetails);
+		// session.getTransaction().commit();
+		// session.close();
+		//
+		//
+		// userDetails=null;
+		// session=sessionFactory.openSession();
+		// session.beginTransaction();
+		// userDetails=(UserDetails) session.get(UserDetails.class, 1);//Here
+		// 1'st parameter is class what we need to retrive
+		// //2'nd parameter is for primary key by which it understand that this
+		// record will be taken.
+		// System.out.println("User Details are as follows : \nUser Name : "+userDetails.getUserName());
 
+		// <<<<<<<<<<<<<<<<<<<<<<<<<<<<*>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+		// After Full these properties siplefy the code.
+
+		UserDetails user1 = new UserDetails();
+		user1.setUserName("First User");
+		UserDetails user2=new UserDetails();
+		user2.setUserName("Second User");
 		SessionFactory sessionFactory = new Configuration().configure()
 				.buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		session.beginTransaction();
-		session.save(userDetails);
+		session.save(user1);
+		session.saveOrUpdate(user2);
 		session.getTransaction().commit();
+		session.close();
 
 	}
 
